95c12b9845bcce7d004da9b588f5abea3d204dd1
mellambias <mellambias@gmail.com>
2024-08-20 18:38:43 +0200

    refactor to async/await

--- temas/p3/controllers/notes.js
+++ temas/p3/controllers/notes.js
@@ -1,62 +1,63 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
 
-notesRouter.get("/", (req, res) => {
-	Note.find({}).then((notes) => {
-		res.json(notes);
-	});
+notesRouter.get("/", async (req, res) => {
+	const notes = await Note.find({});
+	res.json(notes);
 });
 
-notesRouter.get("/:id", (req, res, next) => {
-	Note.findById(req.params.id)
-		.then((note) => {
-			if (note) {
-				res.json(note);
-			} else {
-				res.status(404).end();
-			}
-		})
-		.catch((err) => next(err));
+notesRouter.get("/:id", async (req, res, next) => {
+	try {
+		const note = await Note.findById(req.params.id);
+		if (note) {
+			res.json(note);
+		} else {
+			res.status(404).end();
+		}
+	} catch (err) {
+		next(err);
+	}
 });
 
-notesRouter.delete("/:id", (req, res, next) => {
-	Note.findByIdAndDelete(req.params.id)
-		.then((result) => {
-			res.status(204).end();
-		})
-		.catch((err) => next(err));
+notesRouter.delete("/:id", async (req, res, next) => {
+	try {
+		await Note.findByIdAndDelete(req.params.id);
+		res.status(204).end();
+	} catch (error) {
+		next(error);
+	}
 });
 
-notesRouter.put("/:id", (req, res, next) => {
-	const { important } = req.body;
-
-	Note.findByIdAndUpdate(
-		req.params.id,
-		{ important },
-		{ new: true, runValidators: true },
-	)
-		.then((updatedNote) => {
-			res.json(updatedNote);
-		})
-		.catch((err) => next(err));
+notesRouter.put("/:id", async (req, res, next) => {
+	try {
+		const { important } = req.body;
+		const updatedNote = await Note.findByIdAndUpdate(
+			req.params.id,
+			{ important },
+			{ new: true, runValidators: true },
+		);
+		res.json(updatedNote);
+	} catch (error) {
+		next(error);
+	}
 });
 
-notesRouter.post("/", (req, res, next) => {
-	const note = req.body;
+notesRouter.post("/", async (req, res, next) => {
+	try {
+		const note = req.body;
 
-	const newNote = new Note({
-		content: note.content,
-		important:
-			typeof note.important !== "undefined" ? Boolean(note.important) : false,
-		date: new Date().toISOString(),
-	});
+		const newNote = new Note({
+			content: note.content,
+			important:
+				typeof note.important !== "undefined" ? Boolean(note.important) : false,
+			date: new Date().toISOString(),
+		});
 
-	newNote
-		.save()
-		.then((savedNote) => {
-			res.status(201).json(savedNote);
-		})
-		.catch((error) => next(error));
+		const savedNote = await newNote.save();
+		res.status(201).json(savedNote);
+	} catch (error) {
+		next(error);
+	}
 });
 
 module.exports = notesRouter;
