05ee57b0c8db00bd6cd85b6d4877781f55abdeb5
mellambias <mellambias@gmail.com>
2024-08-30 12:32:49 +0200

    ej 5.3

--- exercises/part5/bloglist-frontend/src/App.jsx
+++ exercises/part5/bloglist-frontend/src/App.jsx
@@ -3,6 +3,70 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
+function LoginForm({ handleLogin, username, setUsername, password, setPassword }) {
+  return (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+}
+
+function CreateBlogForm({ createBlog }) {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+  return (
+    <form onSubmit={() => createBlog(title, author, url)}>
+      <div>
+        title
+        <input
+          type="text"
+          name="Title"
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        author
+        <input
+          type="text"
+          name="Author"
+          value={author}
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url
+        <input
+          type="text"
+          name="Url"
+          value={url}
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <button type="submit">create</button>
+    </form>
+  )
+}
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
@@ -27,6 +91,7 @@ const App = () => {
     try {
       const user = await loginService.login(username, password)
       window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -38,39 +103,33 @@ const App = () => {
   function handleLogout() {
     window.localStorage.removeItem('loggedBlogappUser')
     setUser(null)
+    blogService.setToken(null)
   }
 
-  function LoginForm() {
-    return (
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-          <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
-    )
+  const createBlog = async (title, author, url) => {
+    const newBlog = {
+      title,
+      author,
+      url,
+    };
+    try {
+      const blog = await blogService.create(newBlog);
+      setBlogs(blogs.concat(blog));
+    } catch (error) {
+      console.error("Se ha producido un error", error.response.statusText)
+    }
   }
 
   if (user === null) {
     return (
       <div>
         <h2>log in to application</h2>
-        {LoginForm()}
+        <LoginForm
+          handleLogin={handleLogin}
+          username={username}
+          setUsername={setUsername}
+          password={password}
+          setPassword={setPassword} />
       </div>
     )
   }
@@ -79,6 +138,9 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>{user.name} logged in <button type="button" onClick={handleLogout}>logout</button></p>
+      <div>
+        <CreateBlogForm createBlog={createBlog} />
+      </div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
--- exercises/part5/bloglist-frontend/src/services/blogs.js
+++ exercises/part5/bloglist-frontend/src/services/blogs.js
@@ -12,4 +12,12 @@ const getAll = () => {
 	return request.then((response) => response.data);
 };
 
-export default { getAll, setToken };
+async function create(newBlog) {
+	const config = {
+		headers: { Authorization: token },
+	};
+	const response = await axios.post(baseUrl, newBlog, config);
+	return response.data;
+}
+
+export default { getAll, setToken, create };
