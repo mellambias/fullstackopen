439ed4106c2badedc54d96ffc7f3173cac3071b3
mellambias <mellambias@gmail.com>
2024-09-03 13:28:47 +0200

    eje 5.7

--- exercises/part5/bloglist-frontend/src/components/Blog.jsx
+++ exercises/part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,29 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+const Blog = ({ blog }) => {
+  const [showDetail, setShowDetail] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button type="button" onClick={() => setShowDetail(!showDetail)}>{showDetail ? "hide" : "view"}</button>
+      </div>
+      {showDetail && <>
+        {blog.url}<br />
+        likes {blog.likes} <button type="button">like</button><br />
+        {blog.user?.name}<br />
+      </>}
+    </div>
+  )
+}
 
 export default Blog
